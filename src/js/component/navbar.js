93fe00b81900a2b95ecd50c207826a8dd2e48714
Jasmin Inneh <110208655+JasminInneh@users.noreply.github.com>
2024-02-06 01:35:40 +0000

    created Navbar for Star Wars Reading List Project

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,12 +5,15 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1"><img src="https://www.freepnglogos.com/uploads/star-wars-logo-png-10.png" width="65" height="35" /></span>
 			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+			<div class="dropdown">
+  				<button class="btn btn-warning dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    				Favorites
+  				</button>
+  				<ul class="dropdown-menu">
+    				<li><a class="dropdown-item" href="#">Action</a></li>
+  				</ul>
 			</div>
 		</nav>
 	);
