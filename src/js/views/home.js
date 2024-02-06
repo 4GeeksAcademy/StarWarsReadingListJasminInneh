1234482e0b4a4addd386da2cd92aba2a23a0433b
Jasmin Inneh <110208655+JasminInneh@users.noreply.github.com>
2024-02-06 02:39:46 +0000

    added more information to cards for home page

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -8,7 +8,17 @@ export const Home = () => (
 			<div className="card" style={{ width: '18rem' }}>
 				<img src="..." className="card-img-top" alt="..."/>
 				<div className="card-body">
-					<h5 className="card-title">Card title</h5>
+					<h5 className="card-title">Leia Organa</h5>
+					<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" className="btn btn-primary">Learn more</a>
+				</div>
+			</div>	
+    		</div>
+    		<div className="col">
+			<div className="card" style={{ width: '18rem' }}>
+				<img src="./assets/img/characters/1.jpg" className="card-img-top" alt="..."/>
+				<div className="card-body">
+					<h5 className="card-title">Beru Whitesun Lars</h5>
 					<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 					<a href="#" className="btn btn-primary">Learn more</a>
 				</div>
@@ -18,17 +28,7 @@ export const Home = () => (
 			<div className="card" style={{ width: '18rem' }}>
 				<img src="..." className="card-img-top" alt="..."/>
 				<div className="card-body">
-					<h5 className="card-title">Card title</h5>
-					<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-					<a href="#" className="btn btn-primary">Learn more</a>
-				</div>
-			</div>	
-    		</div>
-    		<div className="col">
-			<div className="card" style={{ width: '18rem' }}>
-				<img src="..." className="card-img-top" alt="..."/>
-				<div className="card-body">
-					<h5 className="card-title">Card title</h5>
+					<h5 className="card-title">C-3PO</h5>
 					<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 					<a href="#" className="btn btn-primary">Learn more</a>
 				</div>
