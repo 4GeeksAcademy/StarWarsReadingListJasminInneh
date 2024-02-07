89be16dccc55a7e4074d65c52f96afe56f9d3598
Jasmin Inneh <110208655+JasminInneh@users.noreply.github.com>
2024-02-07 02:06:47 +0000

    added another row of cards to my home page for the StarWars planets

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,10 +3,12 @@ import "../../styles/home.css";
 
 export const Home = () => (
 	<div className="container text-center">
+		<div className="Characters">
+		<h2 class=" subtitles d-flex p-2 text-warning">Characters</h2>
   		<div className="row">
     		<div className="col">
-			<div className="card" style={{ width: '18rem' }}>
-				<img src="..." className="card-img-top" alt="..."/>
+			<div className="card" style={{ width: '18rem'}}>
+				<img src="https://starwars-visualguide.com/assets/img/characters/5.jpg" className="card-img-top" alt="..."/>
 				<div className="card-body">
 					<h5 className="card-title">Leia Organa</h5>
 					<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -16,7 +18,7 @@ export const Home = () => (
     		</div>
     		<div className="col">
 			<div className="card" style={{ width: '18rem' }}>
-				<img src="./assets/img/characters/1.jpg" className="card-img-top" alt="..."/>
+				<img src="https://starwars-visualguide.com/assets/img/characters/7.jpg" className="card-img-top" alt="..."/>
 				<div className="card-body">
 					<h5 className="card-title">Beru Whitesun Lars</h5>
 					<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -26,7 +28,7 @@ export const Home = () => (
     		</div>
     		<div className="col">
 			<div className="card" style={{ width: '18rem' }}>
-				<img src="..." className="card-img-top" alt="..."/>
+				<img src="https://starwars-visualguide.com/assets/img/characters/2.jpg" className="card-img-top" alt="..."/>
 				<div className="card-body">
 					<h5 className="card-title">C-3PO</h5>
 					<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -35,5 +37,41 @@ export const Home = () => (
 			</div>	
     		</div>
   		</div>
+		</div>
+		<div className="Planets">
+		<h2 class=" subtitles d-flex p-2 text-warning">Planets</h2>
+  		<div className="row">
+    		<div className="col">
+			<div className="card" style={{ width: '18rem' }}>
+				<img src="https://starwars-visualguide.com/assets/img/planets/4.jpg" className="card-img-top" alt="..."/>
+				<div className="card-body">
+					<h5 className="card-title">Hoth</h5>
+					<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" className="btn btn-primary">Learn more</a>
+				</div>
+			</div>	
+    		</div>
+    		<div className="col">
+			<div className="card" style={{ width: '18rem' }}>
+				<img src="https://starwars-visualguide.com/assets/img/planets/5.jpg" className="card-img-top" alt="..."/>
+				<div className="card-body">
+					<h5 className="card-title">Dagobah</h5>
+					<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" className="btn btn-primary">Learn more</a>
+				</div>
+			</div>	
+    		</div>
+    		<div className="col">
+			<div className="card" style={{ width: '18rem' }}>
+				<img src="https://starwars-visualguide.com/assets/img/planets/2.jpg" className="card-img-top" alt="..."/>
+				<div className="card-body">
+					<h5 className="card-title">Alderaan</h5>
+					<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" className="btn btn-primary ">Learn more</a>
+				</div>
+			</div>	
+    		</div>
+  		</div>
+		</div>  
 	</div>
 );
